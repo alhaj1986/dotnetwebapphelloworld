b3d9064463f0cc48070313d69ced877de705aec0
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2025-03-28 13:54:22 +0530

    Update HelloWorldController.cs

--- hello-world-dotnet/Controllers/HelloWorldController.cs
+++ hello-world-dotnet/Controllers/HelloWorldController.cs
@@ -12,7 +12,7 @@ namespace hello_world_dotnet.Controllers
         {
             return new()
             {
-                Message = "Automate all the things!",
+                Message = "Hello-World Dot Net Application!",
                 Timestamp = DateTime.Now
             };
         }
